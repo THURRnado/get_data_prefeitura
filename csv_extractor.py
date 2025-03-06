da639b7e53f780816107a12128f87e13240883db
THURRnado <arthurvieira.trabalho@gmail.com>
2025-03-06 09:20:42 -0300

    Adicionando a leitura e extracao de dados de arquivo csv

---
+++ csv_extractor.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+def csv_reader(csv:str):
+
+    try:
+
+        with open(csv, encoding='latin1') as f:
+            descricao = f.readline().strip().split(';')[0]
+            nome_empresa = descricao.split('~')[1] if '~' in descricao else "Nome não encontrado"
+
+        with open(csv, encoding='latin1') as f:
+            for i, line in enumerate(f):
+                if "Dataemissão" in line:
+                    header_line = i
+                    break
+
+        df = pd.read_csv(csv, encoding='latin1', sep=';', skiprows=header_line, on_bad_lines='skip')
+
+        df.columns = df.columns.str.strip()
+
+        df = df[df['CPF/CNPJ'].notna()]
+
+        df = df.reset_index(drop=True)
+
+        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+        df["Documento"] = df["Documento"].astype(str).str.replace('.', '', regex=False)
+
+        print(f"Descrição: {descricao}")
+        print(f"Nome da empresa: {nome_empresa}")
+        print(df)
+
+        return
+    
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"O arquivo não foi encontrado")
+    except UnicodeDecodeError as e:
+        raise UnicodeDecodeError(f"Não foi possível decodificar o arquivo")
+    except KeyError as e:
+        raise KeyError(f"Coluna não encontrada")
+    except ValueError as e:
+        raise ValueError(f"Problema ao processar os dados")
+    except Exception as e:
+        raise Exception(f"Houve um erro inesperado - {e}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from login_prefeitura import login_prefeitura
 from process_prefeitura import get_pdf
+from csv_extractor import csv_reader
 
 # im_de_teste = 1484176
 
@@ -7,8 +8,10 @@ try:
 
     im = '1484176'
 
-    get_pdf(im)
+    #get_pdf(im)
+
+    csv_reader(csv=r'media\csv\Relatório da consulta.csv')
 
 except Exception as e:
 
-    print(f"Houve o seguinte erro: {e}")
+    print(f"Houve o seguinte erro: {str(e)}")
